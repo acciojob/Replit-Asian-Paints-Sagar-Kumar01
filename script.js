5810f8b0b589f0c2ec508475675bb0f2cbeb9af1
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-10-21 19:21:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let grid-container = document.getElementById("grid-container");
+let block_id = document.getElementById("block_id");
+let colour_id = document.getElementById("colour_id");
+let change_button = document.getElementById("change_button");
+let Reset = document.getElementById("Reset");
+
+change_button.addEventListener("click", function() {
+});
